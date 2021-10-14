ec3720d716f450e7cc9babfc81c630cd751b9cc5
Dobytchick <48864677+Dobytchick@users.noreply.github.com>
2021-10-14 17:31:08 +0600

    Create soundtrans.lua

---
+++ soundtrans.lua
@@ -0,0 +1,16 @@
+TS = {}
+
+function TS.SoundDuration(sDuration)
+    local TEMP_SPLITTABLE = {}
+    table.Add(TEMP_SPLITTABLE, string.Split(sDuration, ":"))
+    local TEMP_SPLITMINUTES = tonumber(TEMP_SPLITTABLE[1]) * 60
+
+    return TEMP_SPLITMINUTES + tonumber(TEMP_SPLITTABLE[2])
+end
+
+--[[
+EXAMPLE
+TS.SoundDuration("14:31")
+
+Out: 871
+]]
